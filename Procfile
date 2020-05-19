60d51a37414dcc98eab8cd9e54a4dada84913c6a
mathieuc22 <mathieu.collet@gmail.com>
2020-05-19 12:26:41 +0200

    change procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:randomnames
+web: gunicorn randomnames:app
--- randomnames.py
+++ randomnames.py
@@ -24,4 +24,4 @@ def index():
     return render_template('index.html', coolname=coolname)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
